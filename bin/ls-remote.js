efd593dfbed28a51dd81ed638050a2ee92146f64
Tim Caswell <tim@creationix.com>
2013-09-13 23:41:47 -0500

    Update ls-remote command to new API

--- bin/ls-remote.js
+++ bin/ls-remote.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
-// Bootstrap the platform to run on node.js
-require('js-git/lib/platform.js')(require('js-git-node-platform'));
-
-// Load the libraries
-var urlParse = require('url').parse;
+var git = require('git-node');
 var program = require('commander');
-var autoProto = require('js-git/protocols/auto.js');
 
 program
-  .version(require('js-git/package.json').version)
   .usage('<url>')
   .parse(process.argv);
 
+if (program.args.length < 1 || program.args.length > 2) {
+  program.outputHelp();
+  process.exit(1);
+}
 
 if (program.args.length != 1) {
   program.outputHelp();
@@ -19,19 +17,13 @@ if (program.args.length != 1) {
 }
 
 var url = program.args[0];
-var opts = urlParse(url);
-if (!opts.protocol) {
-  opts = urlParse("ssh://" + url);
-}
-
-// Do the action
-var connection = autoProto(opts);
-connection.discover(function (err, refs) {
+var remote = git.remote(url);
+remote.discover(function (err, refs) {
   if (err) throw err;
   Object.keys(refs).forEach(function (ref) {
     console.log(refs[ref] + "\t" + ref);
   });
-  connection.close(function (err) {
+  remote.close(function (err) {
     if (err) throw err;
   });
 });
