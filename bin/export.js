78d980ed28273bfc3a046f334074e80df126f94e
Tim Caswell <tim@creationix.com>
2013-09-14 01:15:55 -0500

    Implement export

---
+++ bin/export.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+var git = require('git-node');
+var program = require('commander');
+var fs = require('fs');
+var pathJoin = require('path').join;
+
+program
+  .usage('[options] [--] <dir>')
+  .option('--ref <branch/tag/ref>', 'export from a specefic local branch, tag, or ref')
+  .option('-q', 'Be quiet; don\t show progress')
+  .option('--git-dir <dir>', 'Use custom git dir instead of cwd')
+  .parse(process.argv);
+
+if (program.args.length !== 1) {
+  program.outputHelp();
+  process.exit(1);
+}
+
+var target = program.args[0];
+var ref = program.ref || "HEAD";
+var source = require('./root.js')(program);
+var repo = git.repo(source);
+var read;
+repo.treeWalk(ref, function (err, stream) {
+  if (err) throw err;
+  read = stream.read;
+  return read(onEntry);
+});
+
+function onEntry(err, entry) {
+  if (err) throw err;
+  if (!entry) return;
+  var path = pathJoin(target, entry.path);
+  if (!program.Q) {
+    var colorPath = path.replace(/\//g, "\x1B[1;34m/\x1B[0;34m") + "\x1B[0m";
+    console.log("%s %s", entry.hash, colorPath);
+  }
+  if (entry.type === "tree") {
+    return fs.mkdir(path, onDone);
+  }
+  if (entry.type === "blob") {
+    return fs.writeFile(path, entry.body, onDone);
+  }
+  return read(onEntry);
+}
+
+function onDone(err) {
+  if (err) throw err;
+  return read(onEntry);
+}
