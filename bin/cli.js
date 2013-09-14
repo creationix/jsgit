26d7265d81c0bf2cd8b40ac1e12885a2c808e185
Tim Caswell <tim@creationix.com>
2013-09-14 00:31:10 -0500

    Implement log command

--- bin/cli.js
+++ bin/cli.js
@@ -6,9 +6,9 @@ program
   .version(require('git-node').version)
   .command("ls-remote <url>", "List remote refs")
   .command("clone <url>", "Clone a repository into a new directory")
-  .command("dump <target>", "Dump the current file tree")
-  .command("fetch <url>", "Fetch updates from remote")
   .command("log", "Show local history")
+  .command("export <target>", "Export tree at HEAD as real files to target")
+  .command("fetch <url>", "Fetch updates from remote")
   .command("switch <branch>", "Switch branch HEAD points to")
   .parse(process.argv);
 
---
+++ bin/log.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+var git = require('git-node');
+var program = require('commander');
+var existsSync = require('fs').existsSync;
+var readFileSync = require('fs').readFileSync;
+var path = require('path');
+
+program
+  .usage('[options] [--] [<ref>]')
+  .option('--tree', 'show content trees')
+  .option('--limit <num>', 'stop loading history after num entries')
+  .option('--git-dir <dir>', 'Use custom git dir instead of cwd')
+  .parse(process.argv);
+
+if (program.args.length > 1) {
+  program.outputHelp();
+  process.exit(1);
+}
+if (program.limit) program.limit = parseInt(program.limit, 10);
+
+var ref = program.args[0] || "HEAD";
+var target = path.resolve(process.cwd(), program.gitDir || '.');
+var head = path.join(target, "HEAD");
+if (!(existsSync(head) && (/^ref: /).test(readFileSync(head, "utf8")))) {
+  console.error("Can't find bare repo at %s", target);
+  process.exit(-1);
+}
+
+
+var repo = git.repo(target);
+repo.logWalk(ref, function (err, log) {
+  if (err) throw err;
+  var shallow;
+  return log.read(onRead);
+
+  function onRead(err, commit) {
+    if (err) throw err;
+    if ("limit" in program && !(program.limit--)) return;
+    if (!commit) return logEnd(shallow);
+    if (commit.last) shallow = true;
+    logCommit(commit);
+    if (!program.tree) return log.read(onRead);
+    repo.treeWalk(commit.body.tree, function (err, tree) {
+      if (err) throw err;
+      tree.read(onEntry);
+      function onEntry(err, entry) {
+        if (err) throw err;
+        if (!entry) {
+          return log.read(onRead);
+        }
+        logEntry(entry);
+        return tree.read(onEntry);
+      }
+    });
+  }
+});
+
+function logCommit(commit) {
+  var author = commit.body.author;
+  var message = commit.body.message;
+  console.log("\n\x1B[33mcommit %s\x1B[0m", commit.hash);
+  console.log("Author: %s <%s>", author.name, author.email);
+  console.log("Date:   %s", author.date);
+  console.log("\n    \x1B[32;1m" + message.trim().split("\n").join("\x1B[0m\n    \x1B[32m") + "\x1B[0m\n");
+}
+
+function logEntry(entry) {
+  var path = entry.path.replace(/\//g, "\x1B[1;34m/\x1B[0;34m") + "\x1B[0m";
+  console.log(" %s %s", entry.hash, path);
+}
+
+function logEnd(shallow) {
+  var message = shallow ? "End of shallow record." : "Beginning of history";
+  console.log("\n\x1B[30;1m%s\x1B[0m\n", message);
+}
